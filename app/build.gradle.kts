2a4bdd853c80a533da4f1a25b55eb694cd581b51
noliw <noliw2017@gmail.com>
2024-08-07 03:19:51 -0700

    Setting up buildType configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,15 +13,7 @@ android {
         }
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
+
     buildFeatures {
         compose = true
     }
--- build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -1,4 +1,6 @@
 import com.android.build.api.dsl.ApplicationExtension
+import com.nolawiworkineh.convention.ExtensionType
+import com.nolawiworkineh.convention.configureBuildTypes
 import com.nolawiworkineh.convention.configureKotlinAndroid
 import com.nolawiworkineh.convention.libs
 import org.gradle.api.Plugin
@@ -23,6 +25,11 @@ class AndroidApplicationConventionPlugin: Plugin<Project> {
                 }
 
                 configureKotlinAndroid(this)
+
+                configureBuildTypes(
+                    commonExtension = this,
+                    extensionType = ExtensionType.APPLICATION
+                )
             }
         }
     }
