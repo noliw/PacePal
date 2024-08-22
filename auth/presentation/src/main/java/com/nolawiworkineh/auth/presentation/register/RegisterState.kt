19a6c5a4fdba47b2a70b9939d9a34f2584d5d2e4
noliw <noliw2017@gmail.com>
2024-08-22 01:17:35 -0700

    working on pwd validation state

--- auth/presentation/src/main/java/com/nolawiworkineh/auth/presentation/register/RegisterState.kt
+++ auth/presentation/src/main/java/com/nolawiworkineh/auth/presentation/register/RegisterState.kt
@@ -4,12 +4,15 @@ package com.nolawiworkineh.auth.presentation.register
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.text2.input.TextFieldState
+import com.nolawiworkineh.auth.domain.PasswordValidationState
 
 data class RegisterState(
     val email: TextFieldState = TextFieldState(),
     val isEmailValid: Boolean = false,
     val password: TextFieldState = TextFieldState(),
     val isPasswordVisible: Boolean = false,
-    val confirmPassword: TextFieldState = TextFieldState(),
-    val isConfirmPasswordValid: Boolean = false
+    val passwordValidationState: PasswordValidationState = PasswordValidationState(),
+    // shows the loading indicator when registering
+    val isRegistering: Boolean = false,
+    val canRegister: Boolean = passwordValidationState.isValidPassword && !isRegistering,
 )
