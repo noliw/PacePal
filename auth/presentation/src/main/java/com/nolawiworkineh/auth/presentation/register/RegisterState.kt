a9229189f41946d3cd024c98c753455c798a2b95
noliw <noliw2017@gmail.com>
2024-08-22 01:11:08 -0700

    working on register action and register State

---
+++ auth/presentation/src/main/java/com/nolawiworkineh/auth/presentation/register/RegisterState.kt
@@ -0,0 +1,15 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
+package com.nolawiworkineh.auth.presentation.register
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.text2.input.TextFieldState
+
+data class RegisterState(
+    val email: TextFieldState = TextFieldState(),
+    val isEmailValid: Boolean = false,
+    val password: TextFieldState = TextFieldState(),
+    val isPasswordVisible: Boolean = false,
+    val confirmPassword: TextFieldState = TextFieldState(),
+    val isConfirmPasswordValid: Boolean = false
+)
