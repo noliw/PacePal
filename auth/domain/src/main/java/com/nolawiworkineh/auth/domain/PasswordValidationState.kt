f02430f78ada32e0033ac8240e4827c4654aa74d
noliw <noliw2017@gmail.com>
2024-08-27 02:13:15 -0700

    done with the password validation state

--- auth/domain/src/main/java/com/nolawiworkineh/auth/domain/PasswordValidationState.kt
+++ auth/domain/src/main/java/com/nolawiworkineh/auth/domain/PasswordValidationState.kt
@@ -2,10 +2,14 @@ package com.nolawiworkineh.auth.domain
 
 // **PasswordValidationState Data Class**: Holds the validation state for a password.
 data class PasswordValidationState(
-    val hasMinLength: Boolean = false, // **Minimum Length**: Indicates if the password meets the minimum length requirement.
-    val hasUppercase: Boolean = false, // **Uppercase Letter**: Indicates if the password contains at least one uppercase letter.
-    val hasLowercase: Boolean = false, // **Lowercase Letter**: Indicates if the password contains at least one lowercase letter.
-    val hasNumber: Boolean = false, // **Number**: Indicates if the password contains at least one number.
+    // **Minimum Length**: Indicates if the password meets the minimum length requirement.
+    val hasMinLength: Boolean = false,
+    // **Uppercase Letter**: Indicates if the password contains at least one uppercase letter.
+    val hasUppercase: Boolean = false,
+    // **Lowercase Letter**: Indicates if the password contains at least one lowercase letter.
+    val hasLowercase: Boolean = false,
+    // **Number**: Indicates if the password contains at least one number.
+    val hasNumber: Boolean = false,
 ) {
     // **isValidPassword Property**: Checks if the password meets all validation criteria.
     val isValidPassword: Boolean
