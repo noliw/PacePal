b63dbd6d6c2de8405b6b7bb6c97621169dff6305
noliw <noliw2017@gmail.com>
2024-08-27 02:03:33 -0700

    setting up the project wide text fields

--- auth/presentation/src/main/java/com/nolawiworkineh/auth/presentation/register/RegisterScreen.kt
+++ auth/presentation/src/main/java/com/nolawiworkineh/auth/presentation/register/RegisterScreen.kt
@@ -4,10 +4,31 @@
 package com.nolawiworkineh.auth.presentation.register
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nolawiworkineh.auth.domain.PasswordValidationState
+import com.nolawiworkineh.auth.presentation.R
+import com.nolawiworkineh.core.presentation.designsystem.PacePalGray
 import com.nolawiworkineh.core.presentation.designsystem.PacePalTheme
+import com.nolawiworkineh.core.presentation.designsystem.PacePalWhite
+import com.nolawiworkineh.core.presentation.designsystem.Poppins
+import com.nolawiworkineh.core.presentation.designsystem.components.GradientBackground
+import com.nolawiworkineh.presentation.ui.VerticalSpacer
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -28,7 +49,60 @@ private fun RegisterScreen(
     state: RegisterState,
     onAction: (RegisterAction) -> Unit
 ) {
+    GradientBackground {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+                .padding(vertical = 32.dp)
+                .padding(top = 48.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.create_account),
+                style = MaterialTheme.typography.headlineLarge,
+                color = PacePalWhite
+            )
+            val annotatedString = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        fontFamily = Poppins,
+                        color = PacePalGray
+                    )
+                ) {
+                    append(stringResource(id = R.string.already_have_an_account) + " ")
+                    pushStringAnnotation(
+                        tag = "clickable_text",
+                        annotation = stringResource(id = R.string.login)
+                    )
+                    withStyle(
+                        style = SpanStyle(
+                            fontFamily = Poppins,
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.primary,
+                            
+                        )
+                    ) {
+                        append(stringResource(id = R.string.login))
+                    }
+                }
+            }
+            ClickableText(text = annotatedString,
+                onClick = {
+                    annotatedString.getStringAnnotations(
+                        tag = "clickable_text",
+                        start = it,
+                        end = it
+                    ).firstOrNull()?.let {
+                        onAction(RegisterAction.OnLoginClick)
+                    }
+                }
+            )
+            VerticalSpacer(48)
 
+
+        }
+    }
 }
 
 
@@ -37,7 +111,7 @@ private fun RegisterScreen(
 private fun RegisterScreenRootScreenPreview() {
     PacePalTheme {
         RegisterScreen(
-            state =  RegisterState(
+            state = RegisterState(
                 passwordValidationState = PasswordValidationState(
                     hasNumber = true,
                 )
