d381b368d7b52270c73019726dcdc5eaa79a0990
noliw <noliw2017@gmail.com>
2024-08-28 04:42:20 -0700

    user data validator class

---
+++ auth/domain/src/main/java/com/nolawiworkineh/auth/domain/UserDataValidator.kt
@@ -0,0 +1,10 @@
+package com.nolawiworkineh.auth.domain
+// **UserDataValidator Class**: A utility class for validating user data, particularly passwords.
+class UserDataValidator {
+
+    // **Companion Object**: A block that allows the definition of constants and utility methods associated with the class.
+    companion object {
+        // **Minimum Password Length Constant**: Defines the minimum number of characters required for a valid password.
+        const val MIN_PASSWORD_LENGTH = 9
+    }
+}
--- auth/presentation/src/main/java/com/nolawiworkineh/auth/presentation/register/RegisterScreen.kt
+++ auth/presentation/src/main/java/com/nolawiworkineh/auth/presentation/register/RegisterScreen.kt
@@ -5,30 +5,49 @@ package com.nolawiworkineh.auth.presentation.register
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nolawiworkineh.auth.domain.PasswordValidationState
 import com.nolawiworkineh.auth.presentation.R
+import com.nolawiworkineh.core.presentation.designsystem.CheckIcon
+import com.nolawiworkineh.core.presentation.designsystem.CrossIcon
+import com.nolawiworkineh.core.presentation.designsystem.EmailIcon
+import com.nolawiworkineh.core.presentation.designsystem.PacePalBlue
+import com.nolawiworkineh.core.presentation.designsystem.PacePalDarkRed
 import com.nolawiworkineh.core.presentation.designsystem.PacePalGray
 import com.nolawiworkineh.core.presentation.designsystem.PacePalTheme
 import com.nolawiworkineh.core.presentation.designsystem.PacePalWhite
 import com.nolawiworkineh.core.presentation.designsystem.Poppins
 import com.nolawiworkineh.core.presentation.designsystem.components.GradientBackground
 import com.nolawiworkineh.core.presentation.designsystem.VerticalSpacer
+import com.nolawiworkineh.core.presentation.designsystem.components.PacePalActionButton
+import com.nolawiworkineh.core.presentation.designsystem.components.PacePalPasswordTextField
+import com.nolawiworkineh.core.presentation.designsystem.components.PacePalTextField
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -99,9 +118,98 @@ private fun RegisterScreen(
                 }
             )
             VerticalSpacer(48)
+            PacePalTextField(
+                state = state.email,
+                startIcon = EmailIcon,
+                endIcon = if (state.isEmailValid) {
+                    CheckIcon
+                } else null,
+                hint = stringResource(id = R.string.example_email),
+                title = stringResource(id = R.string.email),
+                modifier = Modifier.fillMaxWidth(),
+                additionalInfo = stringResource(id = R.string.must_be_a_valid_email),
+                keyboardType = KeyboardType.Email
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            PacePalPasswordTextField(
+                state = state.password,
+                isPasswordVisible = state.isPasswordVisible,
+                onTogglePasswordVisibility = {
+                    onAction(RegisterAction.OnTogglePasswordVisibilityClick)
+                },
+                hint = stringResource(id = R.string.password),
+                title = stringResource(id = R.string.password),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
 
-
+            PasswordRequirement(
+                text = stringResource(
+                    id = R.string.at_least_x_characters,
+                    UserDataValidator.MIN_PASSWORD_LENGTH
+                ),
+                isValid = state.passwordValidationState.hasMinLength
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            PasswordRequirement(
+                text = stringResource(
+                    id = R.string.at_least_one_number,
+                ),
+                isValid = state.passwordValidationState.hasNumber
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            PasswordRequirement(
+                text = stringResource(
+                    id = R.string.contains_lowercase_char,
+                ),
+                isValid = state.passwordValidationState.hasLowerCaseCharacter
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            PasswordRequirement(
+                text = stringResource(
+                    id = R.string.contains_uppercase_char,
+                ),
+                isValid = state.passwordValidationState.hasUppercase
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            PacePalActionButton(
+                text = stringResource(id = R.string.register),
+                isLoading = state.isRegistering,
+                enabled = state.canRegister,
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    onAction(RegisterAction.OnRegisterClick)
+                }
+            )
         }
+    }
+}
+
+@Composable
+fun PasswordRequirement(
+    text: String,
+    isValid: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = if (isValid) {
+                CheckIcon
+            } else {
+                CrossIcon
+            },
+            contentDescription = null,
+            tint = if(isValid) PacePalBlue else PacePalDarkRed
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            fontSize = 14.sp
+        )
     }
 }
 
