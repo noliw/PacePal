61c1096da2b31b499425757c3313f260abf788a8
noliw <noliw2017@gmail.com>
2024-09-07 00:53:46 -0700

    setting up the auth repository interface

---
+++ auth/domain/src/main/java/com/nolawiworkineh/auth/domain/AuthRepository.kt
@@ -0,0 +1,20 @@
+package com.nolawiworkineh.auth.domain
+
+import com.nolawiworkineh.core.domain.util.DataError
+import com.nolawiworkineh.core.domain.util.EmptyDataResult
+
+
+// This interface defines what actions the app should be able to perform related to user authentication.
+interface AuthRepository {
+
+    // This function will register a user.
+
+    // It takes an email and password and tries to register the user.
+
+    // It's a 'suspend' function, which means it works asynchronously (in the background)
+    // because registering a user requires communicating with a remote server.
+
+    // The function returns an EmptyDataResult, which tells us if the registration succeeded or failed due to a network issue.
+    suspend fun register(email: String, password: String): EmptyDataResult<DataError.Network>
+}
+
