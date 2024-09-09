fe837da9602dd4c134a3a7309491cf3e9a60580d
noliw <noliw2017@gmail.com>
2024-09-09 01:15:49 -0700

    commenting the coreDataModule

--- core/data/src/main/java/com/nolawiworkineh/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/nolawiworkineh/core/data/di/CoreDataModule.kt
@@ -3,7 +3,10 @@ package com.nolawiworkineh.core.data.di
 import com.nolawiworkineh.core.data.networking.HttpClientFactory
 import org.koin.dsl.module
 
+// **Define the DI module for the auth data layer**:
+// sets up a single instance of the HttpClientFactory
 var coreDataModule = module {
+    // Provides a single instance of the HTTP client by using the HttpClientFactory to build it
 single{
     HttpClientFactory().build()
 }
