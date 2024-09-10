5b4aa4d603fafcc321502dcfc56855fa3a0f4da5
noliw <noliw2017@gmail.com>
2024-09-10 00:41:56 -0700

    commenting on the login action

--- auth/presentation/src/main/java/com/nolawiworkineh/auth/presentation/login/LoginAction.kt
+++ auth/presentation/src/main/java/com/nolawiworkineh/auth/presentation/login/LoginAction.kt
@@ -1,7 +1,14 @@
 package com.nolawiworkineh.auth.presentation.login
 
+// sealed interface that handles all the user actions
 sealed interface LoginAction {
+
+    // **Password Visibility Toggle Action**: Triggered when the user clicks the "show/hide password" button.
     data object onTogglePasswordVisibility : LoginAction
+
+    // **Login Button Click Action**: Triggered when the user clicks the "Login" button.
     data object onLoginClick : LoginAction
-    data object onRegisterClick: LoginAction
+
+    // **Register Button Click Action**: Triggered when the user clicks the "Register" button to navigate to the registration screen.
+    data object onRegisterClick : LoginAction
 }
