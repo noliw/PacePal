f464530f1a3a33f08772f74e6c0559e2924f2d92
noliw <noliw2017@gmail.com>
2024-09-09 19:16:36 -0700

    commenting on the access token response

--- core/data/src/main/java/com/nolawiworkineh/core/data/networking/AccessTokenResponse.kt
+++ core/data/src/main/java/com/nolawiworkineh/core/data/networking/AccessTokenResponse.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AccessTokenResponse (
-    val accessToken: String,
-    val userId: String,
-    val expirationTimestamp: String,
+    val accessToken: String,  // The new access token provided by the server
+    val userId: String,  // The user ID for which the token is issued
+    val expirationTimestamp: String  // The expiration time of the new access token
 )
