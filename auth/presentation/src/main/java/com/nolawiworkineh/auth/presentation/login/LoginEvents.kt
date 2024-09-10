d900e9e2eb350a84eca6b5e6e6d3116c342b3b37
noliw <noliw2017@gmail.com>
2024-09-10 01:13:37 -0700

    commenting on the login events

--- auth/presentation/src/main/java/com/nolawiworkineh/auth/presentation/login/LoginEvents.kt
+++ auth/presentation/src/main/java/com/nolawiworkineh/auth/presentation/login/LoginEvents.kt
@@ -2,7 +2,12 @@ package com.nolawiworkineh.auth.presentation.login
 
 import com.nolawiworkineh.presentation.ui.UiText
 
+// this interface is used to handle the different messages we get from our server to the ui
 sealed interface LoginEvent {
+
+    // **Error Event**: Triggered when there is an error during the login process (e.g., invalid credentials, network error).
     data class Error(val errorMessage: UiText) : LoginEvent
+
+    // **Login Success Event**: Triggered when the login process is successful and the user is authenticated.
     data object LoginSuccess : LoginEvent
 }
