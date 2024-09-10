991d8a58427d54fe82bfd7bd6fdb50c3dcaed1b8
noliw <noliw2017@gmail.com>
2024-09-09 18:46:49 -0700

    creating a singleton instance of the EncryptedSessionStorage class and bind it to the SessionStorage interface

--- core/data/src/main/java/com/nolawiworkineh/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/nolawiworkineh/core/data/di/CoreDataModule.kt
@@ -1,6 +1,10 @@
 package com.nolawiworkineh.core.data.di
 
+import com.nolawiworkineh.core.data.auth.EncryptedSessionStorage
 import com.nolawiworkineh.core.data.networking.HttpClientFactory
+import com.nolawiworkineh.core.domain.SessionStorage
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 // **Define the DI module for the auth data layer**:
@@ -10,4 +14,7 @@ var coreDataModule = module {
     single {
         HttpClientFactory().build()
     }
+
+    // Provides a single instance of the SessionStorage by using the EncryptedSessionStorage
+    singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
