5d5a09b8fc0c126187d51c6542b5d10755f5e720
noliw <noliw2017@gmail.com>
2024-09-12 02:16:21 -0700

    workking on main vm

--- app/src/main/java/com/nolawiworkineh/pacepal/MainViewModel.kt
+++ app/src/main/java/com/nolawiworkineh/pacepal/MainViewModel.kt
@@ -1,7 +1,12 @@
 package com.nolawiworkineh.pacepal
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.nolawiworkineh.core.domain.SessionStorage
+import kotlinx.coroutines.launch
 
 //let's go to the app module and have a view model that is bound to the activity.
        // in here we will have the logic to check if we are already authenticated
@@ -10,4 +15,17 @@ class MainViewModel (
     private val sessionStorage : SessionStorage
 ) : ViewModel() {
 
+    var state by mutableStateOf(MainState())
+    private set
+
+    init {
+        viewModelScope.launch {
+            state = state.copy(isCheckingAuth = true)
+            state = state.copy(
+                isLoggedIn = sessionStorage.get() != null,
+
+            )
+            state = state.copy(isCheckingAuth = false)
+        }
+    }
 }
