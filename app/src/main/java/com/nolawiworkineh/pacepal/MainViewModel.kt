d5055b0d32f5562613c2992cf0eb53a3e675059a
noliw <noliw2017@gmail.com>
2024-09-12 04:40:37 -0700

    commenting main vm

--- app/src/main/java/com/nolawiworkineh/pacepal/MainViewModel.kt
+++ app/src/main/java/com/nolawiworkineh/pacepal/MainViewModel.kt
@@ -8,23 +8,29 @@ import androidx.lifecycle.viewModelScope
 import com.nolawiworkineh.core.domain.SessionStorage
 import kotlinx.coroutines.launch
 
-//let's go to the app module and have a view model that is bound to the activity.
-       // in here we will have the logic to check if we are already authenticated
-
-class MainViewModel (
-    private val sessionStorage : SessionStorage
+// thia class will have the logic to check if we are already authenticated
+class MainViewModel(
+    // This is where we store and retrieve the login info (like the user’s token).
+    private val sessionStorage: SessionStorage
 ) : ViewModel() {
 
+    // This keeps track of whether the user is logged in and whether we’re checking that info.
     var state by mutableStateOf(MainState())
-    private set
+        private set  // Only this ViewModel can change the state.
 
+    //
     init {
+        // We're starting a background task to check if the user is already logged in.
         viewModelScope.launch {
+            // While we're checking, we set the state to show that the check is in progress.
             state = state.copy(isCheckingAuth = true)
-            state = state.copy(
-                isLoggedIn = sessionStorage.get() != null,
 
+            // Here we check if we have the user's login info stored. If we do, they're logged in.
+            state = state.copy(
+                isLoggedIn = sessionStorage.get() != null
             )
+
+            // Now that we’re done checking, we tell the app we're finished.
             state = state.copy(isCheckingAuth = false)
         }
     }
