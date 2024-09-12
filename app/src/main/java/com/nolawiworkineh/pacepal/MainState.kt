6eabe15602b0e6ae1a38b92d0ae8711a68f101b2
noliw <noliw2017@gmail.com>
2024-09-12 03:58:39 -0700

    commenting main state

--- app/src/main/java/com/nolawiworkineh/pacepal/MainState.kt
+++ app/src/main/java/com/nolawiworkineh/pacepal/MainState.kt
@@ -1,6 +1,8 @@
 package com.nolawiworkineh.pacepal
 
+// stores the state current state of the app.
 data class MainState(
-    val isLoggedIn: Boolean = false,
-    val isCheckingAuth: Boolean = false,
+    val isLoggedIn: Boolean = false,  // Tracks if the user is logged in or not.
+    val isCheckingAuth: Boolean = false,  // Tracks if the app is currently checking authentication status.
 )
+
