9763cc57171c20962902bc748714547482591b41
noliw <noliw2017@gmail.com>
2024-09-18 05:55:05 -0700

    commenting the DropdownMenuItem

--- core/presentation/designsystem/src/main/java/com/nolawiworkineh/core/presentation/designsystem/components/util/DropdownMenuItem.kt
+++ core/presentation/designsystem/src/main/java/com/nolawiworkineh/core/presentation/designsystem/components/util/DropdownMenuItem.kt
@@ -2,7 +2,12 @@ package com.nolawiworkineh.core.presentation.designsystem.components.util
 
 import androidx.compose.ui.graphics.vector.ImageVector
 
+// **DropdownMenuItem**: A data class representing a single item in a dropdown menu.
 data class DropdownMenuItem(
+    // **icon**: An ImageVector representing the icon for the dropdown menu item (e.g., a settings or logout icon).
     val icon: ImageVector,
-    val title: String,
+
+    // **title**: A string representing the text that describes the action or option in the dropdown (e.g., "Logout", "Settings").
+    val title: String
 )
+
