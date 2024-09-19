a0b270f0cb0b9692ec724bbaab73aff95928f979
noliw <noliw2017@gmail.com>
2024-09-19 00:22:45 -0700

    commenting on the ActiveRunEvents

--- run/presentation/src/main/java/com/nolawiworkineh/run/presentation/active_run/ActiveRunEvent.kt
+++ run/presentation/src/main/java/com/nolawiworkineh/run/presentation/active_run/ActiveRunEvent.kt
@@ -2,7 +2,13 @@ package com.nolawiworkineh.run.presentation.active_run
 
 import com.nolawiworkineh.presentation.ui.UiText
 
+// events that we send from the viewModel to our screen
 sealed interface ActiveRunEvent {
-    data class Error(val error: UiText): ActiveRunEvent
-    data object RunSaved: ActiveRunEvent
+    // **Error Event**: Represents an error state during the active run, typically used to display error messages to the user.
+    data class Error(val error: UiText) : ActiveRunEvent
+
+    // **RunSaved Event**: This event is triggered when the run data is successfully saved.
+    data object RunSaved : ActiveRunEvent
 }
+
+
