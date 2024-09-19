7c4c1e7c7a0af3e1f88bc853f59ce7e5c3ad5563
noliw <noliw2017@gmail.com>
2024-09-19 01:54:17 -0700

    working on the active run screen

--- run/presentation/src/main/java/com/nolawiworkineh/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/nolawiworkineh/run/presentation/active_run/ActiveRunScreen.kt
@@ -1,8 +1,27 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.nolawiworkineh.run.presentation.active_run
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nolawiworkineh.core.presentation.designsystem.PacePalTheme
+import com.nolawiworkineh.core.presentation.designsystem.StartIcon
+import com.nolawiworkineh.core.presentation.designsystem.StopIcon
+import com.nolawiworkineh.core.presentation.designsystem.components.PacePalFloatingActionButton
+import com.nolawiworkineh.core.presentation.designsystem.components.PacePalScaffold
+import com.nolawiworkineh.core.presentation.designsystem.components.PacePalToolbar
+import com.nolawiworkineh.run.presentation.R
+import com.nolawiworkineh.run.presentation.active_run.components.RunDataCard
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -22,6 +41,47 @@ private fun ActiveRunScreen(
     state: ActiveRunState,
     onAction: (ActiveRunAction) -> Unit
 ) {
+    PacePalScaffold(
+        withGradient = false,
+        topAppBar = {
+            PacePalToolbar(
+                showBackButton = true,
+                title = stringResource(id = R.string.active_run),
+                onBackClick = {
+                    onAction(ActiveRunAction.OnBackClick)
+                },
+            )
+        },
+        floatingActionButton = {
+            PacePalFloatingActionButton(
+                icon = if (state.isTracking) StopIcon else StartIcon,
+                onClick = {
+                    onAction(ActiveRunAction.OnToggleRunClick)
+                },
+                iconSize = 20.dp,
+                contentDescription = if(state.isTracking) {
+                    stringResource(id = R.string.pause_run)
+                } else {
+                    stringResource(id = R.string.start_run)
+                }
+            )
+        }
+    ) { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surface)
+        ) {
+            RunDataCard(
+                elapsedTime = state.elapsedTime,
+                runData = state.runData,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .padding(padding)
+                    .fillMaxWidth()
+            )
+        }
+    }
 
 }
 
