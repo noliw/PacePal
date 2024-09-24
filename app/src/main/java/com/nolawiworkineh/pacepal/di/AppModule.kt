876874fb80a8199f422093d4ce71264d8bbc2547
noliw <noliw2017@gmail.com>
2024-09-23 19:14:51 -0700

    adding the dependency injection in the app modules

--- app/src/main/java/com/nolawiworkineh/pacepal/di/AppModule.kt
+++ app/src/main/java/com/nolawiworkineh/pacepal/di/AppModule.kt
@@ -4,6 +4,8 @@ import android.content.SharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import com.nolawiworkineh.pacepal.MainViewModel
+import com.nolawiworkineh.pacepal.PacePalApp
+import kotlinx.coroutines.CoroutineScope
 import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
@@ -23,6 +25,10 @@ val appModule = module {
             EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,  // Encryption for keys
             EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM  // Encryption for values
         )
+    }
+
+    single<CoroutineScope>{
+        (androidApplication() as PacePalApp).applicationScope
     }
 
     viewModelOf(::MainViewModel)
