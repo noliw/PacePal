76b582b071d1dcf59a827448f2341d75ed2d0b75
noliw <noliw2017@gmail.com>
2024-09-23 21:05:33 -0700

    implemented the running timer logic object

--- core/domain/src/main/java/com/nolawiworkineh/core/domain/Timer.kt
+++ core/domain/src/main/java/com/nolawiworkineh/core/domain/Timer.kt
@@ -6,15 +6,27 @@ import kotlinx.coroutines.flow.flow
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
+// Timer object to emit elapsed time at regular intervals
 object Timer {
+    // Function that tracks time and emits the duration as a Flow
     fun timeAndEmit(): Flow<Duration> {
         return flow {
+            // Capturing the time when the flow starts emitting
             var lastEmitTime = System.currentTimeMillis()
-            while(true) {
+
+            // Infinite loop to continuously emit elapsed time
+            while (true) {
+                // Delay for 200 milliseconds before emitting the next value
                 delay(200L)
+
+                // Get the current time and calculate elapsed time since last emission
                 val currentTime = System.currentTimeMillis()
                 val elapsedTime = currentTime - lastEmitTime
+
+                // Emit the elapsed time in milliseconds as a Kotlin Duration object
                 emit(elapsedTime.milliseconds)
+
+                // Update the lastEmitTime for the next iteration
                 lastEmitTime = currentTime
             }
         }
