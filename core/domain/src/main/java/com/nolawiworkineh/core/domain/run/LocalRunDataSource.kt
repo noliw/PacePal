2952e55f04b78bbadfdff6ab283f8c97be8ea19e
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2024-11-28 00:57:02 -0800

    local data source abstraction

---
+++ core/domain/src/main/java/com/nolawiworkineh/core/domain/run/LocalRunDataSource.kt
@@ -0,0 +1,15 @@
+package com.nolawiworkineh.core.domain.run
+
+import com.nolawiworkineh.core.domain.util.DataError
+import com.nolawiworkineh.core.domain.util.Result
+import kotlinx.coroutines.flow.Flow
+
+typealias RunId = String
+
+interface LocalRunDataSource {
+    fun getRuns(): Flow<List<Run>>
+    suspend fun upsertRun(run: Run): Result<RunId, DataError.Local>
+    suspend fun upsertRuns(runs: List<Run>): Result<List<RunId>, DataError.Local>
+    suspend fun deleteRun(id: String)
+    suspend fun deleteAllRuns()
+}
