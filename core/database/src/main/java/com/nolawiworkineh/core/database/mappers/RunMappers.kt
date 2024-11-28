984bba6d7cbb576afcb19c0832b8d09a9726e6c6
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2024-11-28 01:41:12 -0800

    localroom local run data source

---
+++ core/database/src/main/java/com/nolawiworkineh/core/database/mappers/RunMappers.kt
@@ -0,0 +1,41 @@
+package com.nolawiworkineh.core.database.mappers
+
+import com.nolawiworkineh.core.database.entity.RunEntity
+import com.nolawiworkineh.core.domain.location.Location
+import com.nolawiworkineh.core.domain.run.Run
+import org.bson.types.ObjectId
+import java.time.Instant
+import java.time.ZoneId
+import kotlin.time.Duration.Companion.milliseconds
+
+fun RunEntity.toRun(): Run {
+    return Run(
+        id = id,
+        duration = durationMillis.milliseconds,
+        dateTimeUtc = Instant.parse(dateTimeUtc)
+            .atZone(ZoneId.of("UTC")),
+        distanceMeters = distanceMeters,
+        location = Location(
+            latitude = latitude,
+            longitude = longitude
+        ),
+        maxSpeedKmh = maxSpeedKmh,
+        totalElevationMeters = totalElevationMeters,
+        mapPictureUrl = mapPictureUrl
+    )
+}
+
+fun Run.toRunEntity(): RunEntity {
+    return RunEntity(
+        id = id ?: ObjectId().toHexString(),
+        durationMillis = duration.inWholeMilliseconds,
+        maxSpeedKmh = maxSpeedKmh,
+        dateTimeUtc = dateTimeUtc.toInstant().toString(),
+        latitude = location.latitude,
+        longitude = location.longitude,
+        distanceMeters = distanceMeters,
+        avgSpeedKmh = avgSpeedKmh,
+        totalElevationMeters = totalElevationMeters,
+        mapPictureUrl = mapPictureUrl
+    )
+}
